a09c68288e5be1bc8f3e44c332be4b4a9b2f3b4d
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-07-26 12:49:36 +0700

    modified file

--- youtube-app/ConstantsTemplate.swift
+++ youtube-app/ConstantsTemplate.swift
@@ -1,16 +1,18 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-app
 //
-//  Created by Uwais Alqadri on 25/07/20.
+//  Created by Uwais Alqadri on 26/07/20.
 //  Copyright © 2020 Uwais Alqadri. All rights reserved.
 //
 
 import Foundation
-
+/*
 struct Constants {
     
-    static var API_KEY = ""
-    static var PLAYLIST_ID = "UUgFvT6pUq9HLOvKBYERzXSQ"
+    static var API_KEY = "YOUR APIKEY"
+    static var PLAYLIST_ID = "YOUR PLAYLIST ID"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
+ 
+ */
